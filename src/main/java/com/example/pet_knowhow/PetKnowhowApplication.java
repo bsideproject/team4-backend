4bb5ae95c6dc65e3dc1e18196a0176d9589a17e9
Jinri <54436297+zzznly@users.noreply.github.com>
2022-07-18 21:01:09 +0900

    Add files via upload
    spring generate

---
+++ src/main/java/com/example/pet_knowhow/PetKnowhowApplication.java
@@ -0,0 +1,13 @@
+package com.example.pet_knowhow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetKnowhowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetKnowhowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pet_knowhow/PetKnowhowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_knowhow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetKnowhowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
