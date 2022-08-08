d3f728f2203e624cef4246f8241c91cbdeecc157
sirzzang <sirzzang@naver.com>
2022-08-08 22:00:18 +0900

    fix: security configuration 주석 해제 복구

--- src/main/java/com/bside/sidefriends/security/SecurityConfiguration.java
+++ src/main/java/com/bside/sidefriends/security/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+//        http.csrf().disable();
 
         http
             .csrf().disable()
