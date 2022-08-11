4807219a443ed70d68721c8cac031c1d381fd9e9
sirzzang <sirzzang@naver.com>
2022-08-11 23:56:19 +0900

    feat: 클라이언트에게 반환할 응답 객체 공통 양식 정의 및 응답 코드 enum 클래스 정의

---
+++ src/main/java/com/bside/sidefriends/common/response/ResponseCode.java
@@ -0,0 +1,25 @@
+package com.bside.sidefriends.common.response;
+
+public enum ResponseCode {
+
+    // 로그인(101-150)
+
+    // 회원(151-199)
+
+    // 반려동물(201-299)
+
+    // 퀵 기록(301-399)
+
+    // 체크리스트(401-450)
+
+    // 할 일, 스케쥴(451-499)
+
+    // 양육일지(501-599)
+
+    // 가족 그룹(601-699)
+
+    ;
+
+    private String code;
+    private String message;
+}
