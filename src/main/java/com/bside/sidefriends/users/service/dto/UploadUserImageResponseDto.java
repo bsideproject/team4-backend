4d4d204711279e498f72f665f2f4ce3087983e77
sirzzang <sirzzang@naver.com>
2022-08-29 00:21:20 +0900

    feat: response dto에서 deleted 속성 삭제

--- src/main/java/com/bside/sidefriends/users/service/dto/UploadUserImageResponseDto.java
+++ src/main/java/com/bside/sidefriends/users/service/dto/UploadUserImageResponseDto.java
@@ -2,14 +2,18 @@ package com.bside.sidefriends.users.service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 public class UploadUserImageResponseDto {
 
+    @NotNull
     Long userId;
 
-    @Getter
+    @URL
     String imageUrl;
 
 }
