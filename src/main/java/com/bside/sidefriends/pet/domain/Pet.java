32f7fb3f87d954797f2aaeabff72978e849e74e3
sirzzang <sirzzang@naver.com>
2022-09-03 16:32:11 +0900

    feat: Pet 도메인 엔티티 속성 수정

--- src/main/java/com/bside/sidefriends/pet/domain/Pet.java
+++ src/main/java/com/bside/sidefriends/pet/domain/Pet.java
@@ -2,8 +2,11 @@ package com.bside.sidefriends.pet.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -18,15 +21,19 @@ public class Pet {
     // 사용자
 
     // 공유 타입
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PetShareScope shareScope;
 
     // 이름
+    @Column(nullable = false)
     private String name;
 
     // 품종
     private String breed;
 
     // 성별
+    @Enumerated(EnumType.STRING)
     private PetGender gender;
 
     // 생일
@@ -38,16 +45,26 @@ public class Pet {
     // 나이
     private Long age;
 
-    // 기록 중지 여부
-
-    // 삭제 여부
-
-    // 생성 시각
-
-    // 수정 시각
-
     // 동물등록번호
     private String animalRegistrationNumber;
+
+    // 기록 중지 여부
+    @Column(nullable = false)
+    private boolean isDeactivated; // 초깃값 false
+
+    // 삭제 여부
+    @Column(nullable = false)
+    private boolean isDeleted; // default false
+
+    // 생성 시각
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // 수정 시각
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
     // 질환
     // TODO: 질환 enum 관리 여부
