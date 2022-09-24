7e4368a2cc61ab0d4407cbda04d2f5012770cbba
sirzzang <sirzzang@naver.com>
2022-09-24 12:14:17 +0900

    feat: 한줄일기 도메인 속성 수정

--- src/main/java/com/bside/sidefriends/diary/domain/Diary.java
+++ src/main/java/com/bside/sidefriends/diary/domain/Diary.java
@@ -14,7 +14,9 @@ public class Diary {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long diaryId;
 
-    private Long writerId;
+    private Long writerId; // TODO: 작성자 삭제 및 탈퇴 여부 확인 용도
+
+    private Long writerName;
 
     @Column(length = 140) // TODO: 140자 영문 한글 기준 확인 필요. IR.
     private String contents;
