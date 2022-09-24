3ccfb9212655bcf27d3ab372fc0ab0446138a284
sirzzang <sirzzang@naver.com>
2022-09-24 13:24:37 +0900

    feat: BusinessExceptionHandler 베이스 패키지 클래스 추가

--- src/main/java/com/bside/sidefriends/common/exception/BusinessExceptionHandler.java
+++ src/main/java/com/bside/sidefriends/common/exception/BusinessExceptionHandler.java
@@ -2,6 +2,9 @@ package com.bside.sidefriends.common.exception;
 
 import com.bside.sidefriends.common.response.ResponseCode;
 import com.bside.sidefriends.common.response.ResponseDto;
+import com.bside.sidefriends.family.controller.FamilyController;
+import com.bside.sidefriends.pet.controller.PetController;
+import com.bside.sidefriends.users.controller.UserController;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice()
+@ControllerAdvice(basePackageClasses = {
+        UserController.class,
+        FamilyController.class,
+        PetController.class
+})
 public class BusinessExceptionHandler {
 
     // TODO: Map 사용하는 에러 처리 방식 좋은 방법인지 고민해 볼 것
