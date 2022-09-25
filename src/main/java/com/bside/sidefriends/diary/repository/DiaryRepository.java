b4a598f8cfcf2d91f05a769d7b7d6ac0231476f7
sirzzang <sirzzang@naver.com>
2022-09-25 15:52:35 +0900

    feat: 한줄일기 repository 메서드 오류 수정

--- src/main/java/com/bside/sidefriends/diary/repository/DiaryRepository.java
+++ src/main/java/com/bside/sidefriends/diary/repository/DiaryRepository.java
@@ -6,15 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
-    @Query("SELECT d FROM DIARY d" +
+    @Query("SELECT d FROM Diary d" +
             " WHERE d.pet.petId = :petId")
     List<Diary> findAllByPetId(@Param("petId") Long petId);
 
-    @Query("SELECT d FROM DIARY d" +
+    @Query("SELECT d FROM Diary d" +
             " WHERE d.pet.petId = :petId" +
             " AND d.pet.isDeactivated = false")
     List<Diary> findAllByPetIdAndPetIsDeactivatedFalse(@Param("petId") Long petId);
