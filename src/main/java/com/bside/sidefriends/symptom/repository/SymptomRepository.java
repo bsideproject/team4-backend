2118a2e2166b0ddeafc2dcf5454b1523d2fa186b
sirzzang <sirzzang@naver.com>
2022-10-04 23:07:31 +0900

    feat: 이상징후 기록 repository

--- src/main/java/com/bside/sidefriends/symptom/repository/SymptomRepository.java
+++ src/main/java/com/bside/sidefriends/symptom/repository/SymptomRepository.java
@@ -3,5 +3,11 @@ package com.bside.sidefriends.symptom.repository;
 import com.bside.sidefriends.symptom.domain.Symptom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface SymptomRepository extends JpaRepository<Symptom, Long> {
+
+    Optional<Symptom> findByPetPetIdAndDate(Long PetId, LocalDate date);
 }
