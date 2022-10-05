5a8e28c608409378126e8e6e1c87d3a28ba2481c
sirzzang <sirzzang@naver.com>
2022-10-05 14:41:38 +0900

    feat: 이상징후 기록 수정 시 요청 DTO에서 날짜 제외

--- src/main/java/com/bside/sidefriends/symptom/service/dto/ModifyPetSymptomRequestDto.java
+++ src/main/java/com/bside/sidefriends/symptom/service/dto/ModifyPetSymptomRequestDto.java
@@ -3,14 +3,10 @@ package com.bside.sidefriends.symptom.service.dto;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 public class ModifyPetSymptomRequestDto {
-
-    @NotNull
-    LocalDate date;
 
     @NotNull
     List<String> symptoms;
